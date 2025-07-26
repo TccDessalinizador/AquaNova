3e04b68c0d0d3ec514bd6a5778bd8b68ef482b22
EduardoMVasconcelos <142501013+EduardoMVasconcelos@users.noreply.github.com>
2025-07-25 23:30:14 -0300

    login quase pronto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Login } from './components/login.jsx'
+
 
 export function App() {
   return (
     <div>
-      <p class="bg-blue-500">OI, oi</p>
+      <Login />
     </div>
   )
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,27 @@
+import LogoSite  from "../assets/logo tcc-black 1.jpg"
+
+export function Login() {
+    return (
+        <div class="bg-white flex flex-col">
+            <img src={LogoSite} alt="logo do AquaNova" class="flex self-center justify-center w-[10rem] h-[10rem]"/>
+            <form action="" class="flex flex-col px-[1.63rem] ">
+                <label htmlFor="login" class="text-3x1 mx-[0.5rem]">Login</label>
+                <input type="email" placeholder="Email ou telefone" id="login" class="h-[3.35rem] w-100% mb-[1rem] bg-[#D9D9D9] rounded-[1.125rem] px-[1rem]"/>
+
+                <label htmlFor="senha" class="text-3x1 mx-[0.5rem]">Senha</label>
+                <input type="password" placeholder="Insira a senha" id="senha" class="bg-[#D9D9D9] h-[3.35rem] w-100% mb-[1rem] rounded-[1.125rem] px-[1rem]"/>
+
+                {/* Falta toogle */}
+                <span class="mb-[3.7rem] text-3x1">Lembre-me</span>
+
+                <button class="flex items-center self-center justify-center bg-[#0C0C0C] text-white w-[11.6875rem] h-[2.35rem] mb-[2rem]">Login</button>
+
+                <div id="ancoras" class="items-center self-center">
+                    <a href="#" class="flex justify-center mb-[2rem]">Esqueceu a senha?</a>
+
+                    <p>Não tem uma conta? <a href="#" class="text-[#098DB2]">Registre-se</a></p>
+                </div>
+            </form>
+        </div>
+    )
+}
