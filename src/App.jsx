278233d17b3cb3b4d08f88354e316838ff2fab62
EduardoMVasconcelos <142501013+EduardoMVasconcelos@users.noreply.github.com>
2025-07-25 21:59:56 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+export function App() {
+  return (
+    <div>
+      <p class="bg-blue-500">OI</p>
+    </div>
+  )
+}
