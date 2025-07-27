6c3d0740abea6290b9acf2da19d04e3241f38f03
EduardoMVasconcelos <142501013+EduardoMVasconcelos@users.noreply.github.com>
2025-07-27 12:59:44 -0300

    primeiro commit pagina QuemSomos

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import { Cadastro } from './components/Cadastro.jsx'
 import { Profile } from './components/Profile.jsx'
 import { Jogo } from './components/Jogo.jsx'
 import { Header } from './components/Header.jsx'
+import { QuemSomos } from './components/QuemSomos.jsx'
 
 export function App() {
   return (
     <div>
-      <Jogo />
+      <QuemSomos />
     </div>
   )
 }
