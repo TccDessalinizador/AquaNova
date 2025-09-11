33da1f1029f48eda808d9c4e2fae55c627f8b6a6
EduardoMVasconcelos <142501013+EduardoMVasconcelos@users.noreply.github.com>
2025-09-11 19:01:43 -0300

    ajustes em paginas

--- src/components/CompComponentes.jsx
+++ src/components/CompComponentes.jsx
@@ -9,21 +9,21 @@ export function CompComponentes() {
     const { theme } = useContext(ThemeContext)
     return (
         <div id="componentes" className="bg-white text-white flex flex-col items-center px-[1.5rem] mt-[3rem] dark:bg-[#0C0C0C]">
-            <div id="top" class="mb-[2rem] flex flex-col justify-end text-[#0C0C0C]">
-                <h1 class="flex flex-row text-[1.2rem] lg:text-[2.5rem] font-inter font-bold gap-[.2rem] dark:text-white">Componentes e investimentos <img src={theme === "dark" ? smile : emojiSmileDark} alt="" className="lg:w-[2rem] lg:h-[1.7rem] lg:mt-[1rem]" /></h1>
-                <img src={theme === "dark" ? sublinhado : sublinhadoDark} alt="" class="flex self-end" />
+            <div id="top" class="mb-[2rem] flex flex-col justify-end text-[#0C0C0C] lg:w-[100vw] w-[22rem]">
+                <h1 class="w-[100vw] flex flex-row text-[1.4rem] justify-center items-center lg:text-[2.5rem] font-inter font-bold gap-[.2rem] dark:text-white">Componentes e investimentos <img src={theme === "dark" ? smile : emojiSmileDark} alt="" className="lg:w-[2rem] lg:h-[1.7rem] lg:mt-[1rem] w-[1.5rem] h-[1.5rem]" /></h1>
+                <img src={theme === "dark" ? sublinhado : sublinhadoDark} alt="" class="flex self-center lg:ml-[25rem] ml-[10rem]" />
             </div>
 
             <div class="flex flex-col items-center mb-[3rem] text-[#0C0C0C]">
                 <h2 class="text-[1.5rem] lg:text-[2rem] font-inter font-bold dark:text-white">Saiba onde comprar </h2>
-                <h2 class="text-[1.5rem] lg:text-[2rem] font-inter font-bold dark:text-white ml-[2.8rem] lg:ml-0">e quanto será seu investimento</h2>
+                <h2 class="text-[1.5rem] pl-[1.5rem] lg:text-[2rem] font-inter font-bold dark:text-white ml-[2.8rem] lg:ml-0">e quanto será seu investimento</h2>
             </div>
 
             <div id='video' class="w-[20rem] h-[14rem] lg:w-[50rem] lg:h-[30rem] bg-[#C4C4C4] text-black flex items-center justify-center mb-[2rem] lg:mb-[4rem]"><p>video</p></div>
 
-            <h2 class="text-[1.5rem] text-[#0C0C0C] font-inter font-bold flex justify-center mb-[1.75rem] dark:text-white">Lista de Itens</h2>
+            <h2 class="text-[2rem] text-[#0C0C0C] font-inter font-bold flex self-center justify-center mb-[1.75rem] dark:text-white">Lista de Itens</h2>
 
-            <div id="lista" class="text-[#0C0C0C] flex flex-col lg:flex-row lg:gap-[50rem] w-[100%] mb-[2rem] lg:mb-[4rem]">
+            <div id="lista" class="text-[#0C0C0C] flex flex-col lg:flex-row lg:gap-[50rem] w-[100%] mb-[2rem] lg:mb-[4rem] px-[1rem]">
 
                 <div id="left" class="text-[1.25rem] lg:text-[1.5rem] font-bold font-inter dark:text-white">
                     <p>
--- src/pages/Componentes.jsx
+++ src/pages/Componentes.jsx
@@ -19,7 +19,7 @@ export function Componentes() {
         <div>
             <Header />
             {/* bg-[#0C0C0C] */}
-            <div id="pagina" className="bg-white text-white pt-[4rem] flex flex-col items-center px-[1.5rem] mt-[4rem] dark:bg-[#0C0C0C]">
+            <div id="pagina" className="bg-white text-white pt-[4rem] flex flex-col items-center px-[1.5rem] mt-[4rem] dark:bg-[#0C0C0C] w-[100vw]">
                 <CompComponentes />
 
                 <h2 className="text-[2rem] font-inter font-bold flex justify-center mb-[1rem] text-[#0C0C0C] dark:text-white">Comprar</h2>
@@ -38,7 +38,7 @@ export function Componentes() {
                     <div id="produto2" className="border-[4px] border-[#0C0C0C]  h-[40rem] bg-white mb-[1rem]">
                         <img src={carcaca} alt="" className="w-[22rem] h-[26rem] mb-[2.5rem] mx-auto"/>
 
-                        <h2 className="text-[#0C0C0C] text-[2rem] flex justify-center text-center mb-[.3rem] font-bold">Carcaça para Membrana</h2>
+                        <h2 className="text-[#0C0C0C] text-[1.6rem] flex justify-center text-center mb-[.3rem] font-bold lg:text-[2rem]" >Carcaça para Membrana</h2>
                         <p className="text-[#0C0C0C] text-[1.5rem] flex justify-center font-bold mb-[.5rem]">R$60,91</p>
 
                         <button className="w-[11rem] h-[3.5rem] font-bold text-[1.375rem] text-[#0C0C0C] border border-[#0C0C0C] flex items-center justify-center self-center mx-auto">Ver mais</button>
@@ -48,7 +48,7 @@ export function Componentes() {
                     <div id="produto3" className="border-[4px] border-[#0C0C0C] h-[40rem] bg-white mb-[1rem]">
                         <img src={controlador} alt="" className="w-[24rem] h-[28rem] mb-[.5rem] mx-auto"/>
 
-                        <h2 className="text-[#0C0C0C] text-[2rem] flex justify-center text-center mb-[.3rem] font-bold">Controlador de Carga</h2>
+                        <h2 className="text-[#0C0C0C] text-[1.8rem] flex justify-center text-center mb-[.3rem] font-bold lg:text-[2rem]">Controlador de Carga</h2>
                         <p className="text-[#0C0C0C] text-[1.5rem] flex justify-center font-bold mb-[.5rem]">R$89,00</p>
 
                         <button className="w-[11rem] h-[3.5rem] font-bold text-[1.375rem] text-[#0C0C0C] border border-[#0C0C0C] flex items-center justify-center self-center mx-auto">Ver mais</button>
